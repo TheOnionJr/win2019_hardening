65095c7b08956ce5860c9116c406cebbe855ead3
Erlend Einmo <erlenein@stud.ntnu.no>
2018-10-26 15:27:04 +0000

    ADDED: classes

---
+++ manifests/config.pp
@@ -0,0 +1,8 @@
+# A description of what this class does
+#
+# @summary A short summary of the purpose of this class
+#
+# @example
+#   include win2019_hardening::config
+class win2019_hardening::config {
+}
