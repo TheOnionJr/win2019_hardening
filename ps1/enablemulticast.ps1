c2b230e44dc734ddf886b106b8cf4412096a7996
unknown <sanderlb@stud.ntnu.no>
2018-11-01 18:22:26 +0100

    Added admin mode check and DNS key check

--- ps1/enablemulticast.ps1
+++ ps1/enablemulticast.ps1
@@ -1,2 +1,12 @@
-New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT" -name "DNSClient"
+#Checking if running in admin mode
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")){   
+	$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+	Start-Process powershell -Verb runAs -ArgumentList $arguments
+	Break
+}
+#Checking if DNSClinet registry key exists:
+if (-Not(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\DNSClient")) {
+	New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT" -name "DNSClient"
+}
+#Disabeling multicast
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\DNSClient" -Name "EnableMulticast" -Value 0
